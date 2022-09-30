f571e83597807da32eb07a961a0559f9e81816b2
nithin-gith <nithin.siriguppa@gmail.com>
2022-09-30 13:12:10 +0530

    added two extra end points

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-
-require('dotenv').config()
-
 const app = express()
 app.use(express.json())
 
+require('dotenv').config()
 const port = process.env.PORT || 5001
 
+//import db model
 const Student = require('./models/student')
-
 
 //connecting to db
 mongoose.connect(process.env.URI,
@@ -18,10 +16,12 @@ mongoose.connect(process.env.URI,
     (err)=>console.log(err)
 )
 
+
 // home route (not required)
 app.get('/', (req, res) => {
     res.send('<h1>This is an attendance Tracker API</h1>')
 })
+
 
 // to add the student to the database
 app.post('/add',(req,res)=>{
@@ -48,12 +48,23 @@ app.delete('/delete/:id',(req,res)=>{
 })
 
 
+// to update the whole data of a particular student
+// assuming request body is just student object
+app.post('/update/:id',(req,res)=>{
+    const id = req.params.id
+    const student = req.body
+    Student.findByIdAndUpdate(id,student)
+    .then((data)=>{
+        res.send(student)
+        console.log('Updated successfully!')
+    })
+    .catch(err=>res.status(500).json({ success: false, msg: `Something went wrong. ${err}` }))
+})
 
 
 // to update the attendance of a particular student 
 // req body is information whether the student is present or absent
 // for ex: {present:true} or {present:false}
-
 app.post('/markattendance/:id',(req,res)=>{
     const id = req.params.id
     const present = req.body.present
@@ -67,6 +78,29 @@ app.post('/markattendance/:id',(req,res)=>{
         Student.updateOne({_id:id},data)
         .then(()=>{res.status(200).json({ success: true, msg: `Updated successfully!` });console.log('Updated successfully!')})
         .catch(err=>{res.status(500).json({ success: false, msg: `Something went wrong. ${err}` });return})
+    })
+    .catch(err=>res.status(500).json({ success: false, msg: `Something went wrong. ${err}` }))
+})
+
+
+// to get the attendance of a particular student
+app.get('/getattendance/:id',(req,res)=>{
+    const id = req.params.id
+    Student.findById(id)
+    .then((data)=>{
+        res.send({present:data.present,absent:data.absent})
+        console.log(data)
+    })
+    .catch(err=>res.status(500).json({ success: false, msg: `Something went wrong. ${err}` }))
+})
+
+
+// to get the attendance of all the students
+app.get('/getallattendance',(req,res)=>{
+    Student.find()
+    .then((data)=>{
+        res.send(data)
+        console.log(data)
     })
     .catch(err=>res.status(500).json({ success: false, msg: `Something went wrong. ${err}` }))
 })
